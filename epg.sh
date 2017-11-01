639c3e34b67c9532782b5337c5254da8c3b1ca85
raptor75 <raptor75a@gmail.com>
2017-11-01 17:08:31 +0100

    adding script file

---
+++ epg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /data/legalfreeiptv
+git pull
+
+/data/epg/.wg++/run.sh
+
+#wget https://www.teleguide.info/download/new3/xmltv.xml.gz
+#gunzip xmltv.xml.gz
+#rm guide.xml
+#mv xmltv.xml guide.xml
+
+git add guide.xml
+git commit -m "update epg"
+git push -u origin master
+
